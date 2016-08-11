65e82aca71740650c144449862baac2d71683b9c
xuzm <huahouye@gmail.com>
2016-08-11 20:08:34 +0800

    first commit!

---
+++ src/main/java/com/huahouye/solrj/example/SolrJCloudSolrClientExample.java
@@ -0,0 +1,34 @@
+package com.huahouye.solrj.example;
+
+import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CloudSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.params.ModifiableSolrParams;
+
+/**
+ * CloudSolrClient 连接 SolrCloud 的例子
+ * @author xuzm
+ *
+ */
+public class SolrJCloudSolrClientExample {
+
+	public static void main(String[] args) throws SolrServerException, IOException {
+		// 如果 zookeeper 使用了 chroot 的话，如果配置 Solr 的时候给他的值是 solr，那么这个参数后面要追加 /solr
+		// 类似：String zkHostString = "192.168.191.4:2181,192.168.191.4:2182/solr";
+		String zkHostString = "192.168.191.4:2181,192.168.191.4:2182";
+		SolrClient solr = new CloudSolrClient.Builder().withZkHost(zkHostString).build();
+		
+		ModifiableSolrParams params = new ModifiableSolrParams();
+		params.set("qt", "/select");
+		params.set("q", "id:2");
+		params.set("fl", "id,responseCode");
+
+		// SolrCloud 模式先要给定第一个参数，表示对吗一个 collection 进行操作，否者会报错。
+		QueryResponse response = solr.query("status-center", params);
+		System.out.println("response = " + response);
+	}
+
+}
---
+++ src/main/java/com/huahouye/solrj/example/SolrJHttpSolrClientExample.java
@@ -0,0 +1,29 @@
+package com.huahouye.solrj.example;
+
+import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.params.ModifiableSolrParams;
+
+/**
+ * HttpSolrClient 的例子
+ * @author xuzm
+ *
+ */
+public class SolrJHttpSolrClientExample {
+  public static void main(String[] args) throws SolrServerException, IOException {
+	  String urlString = "http://192.168.191.4:8983/solr/status-center";
+	  SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+
+    // http://localhost:8983/solr/spellCheckCompRH?q=epod&spellcheck=on&spellcheck.build=true
+    ModifiableSolrParams params = new ModifiableSolrParams();
+    params.set("qt", "/select");
+    params.set("q", "id:2");
+
+    QueryResponse response = solr.query(params);
+    System.out.println("response = " + response);
+  }
+}
